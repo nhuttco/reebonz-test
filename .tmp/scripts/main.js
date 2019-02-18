7b79b8488ab113c6e78248299769ddc647a40ae3
Nhựt Trương Công <nhut.truong@enouvo.com>
2019-02-18 11:11:00 +0700

    update font family

--- .tmp/scripts/main.js
+++ .tmp/scripts/main.js
@@ -1,20 +1,20 @@
-$(document).ready(function() {
+$(document).ready(function () {
   const mySwiper = new Swiper('.hero-banner-slider', {
-      direction: 'horizontal',
-      loop: true,
-      autoplay: {
-        delay: 5000
-      },
-      pagination: {
-        el: '.swiper-pagination',
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      scrollbar: {
-        el: '.swiper-scrollbar',
-      },
+    direction: 'horizontal',
+    loop: true,
+    autoplay: {
+      delay: 5000
+    },
+    pagination: {
+      el: '.swiper-pagination'
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+    scrollbar: {
+      el: '.swiper-scrollbar'
+    }
   });
   const swiper = new Swiper('.trend-list-slider', {
     slidesPerView: 6,
@@ -27,14 +27,18 @@ $(document).ready(function() {
     loopFillGroupWithBlank: true,
     pagination: {
       el: '.swiper-pagination',
-      clickable: true,
+      clickable: true
     },
     navigation: {
       nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+      prevEl: '.swiper-button-prev'
+    }
   });
-  const renderNewEvent = ({ name, desc },index) => {
+
+  const renderNewEvent = ({
+    name,
+    desc
+  }, index) => {
     const style_format = [4, 8, 4, 4, 4, 8, 4, 4, 4, 4];
     const col = index % style_format.length;
     return `<div class="col-md-${style_format[col]} col-sm-${style_format[col]} item">
@@ -42,11 +46,12 @@ $(document).ready(function() {
       <img src="images/img_event.jpg" alt="brand-focus">
     </div>
     <div class="info">
-        <p class="title text-semibold">${name}</p>
+        <p class="title">${name}</p>
         <p class="desc">${desc}</p>
     </div>
     </div>`;
   };
+
   const render = (data, selector, func) => {
     $.get(data).then(response => {
       response.forEach((ele, index) => {
@@ -54,5 +59,7 @@ $(document).ready(function() {
       });
     });
   };
+
   render('data/data_events.json', '.event-list', renderNewEvent);
-})
+});
+//# sourceMappingURL=main.js.map
